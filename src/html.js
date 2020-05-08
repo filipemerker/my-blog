21f4d9941f3692edb74029be51278abef700debc
Filipe Merker <fmerk@softserveinc.com>
2020-05-08 13:45:47 +0200

    Remove onesignal

--- src/html.js
+++ src/html.js
@@ -11,7 +11,6 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
